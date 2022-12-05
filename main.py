7742c3d1ed58c382b61ec482b428caf10b0d39db
Sebastian Mendez <mendezsebas8@gmail.com>
2022-12-05 14:57:06 -0500

    cords

--- main.py
+++ main.py
@@ -50,6 +50,22 @@ def full_money(pagos,moras,instancia,data):
 
 app=FastAPI()
 
+"""
+CORDS
+"""
+origins = [
+    "http://localhost:3000",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 @app.get("/")
 def home():
     return {"BIOS":"GLOBAL"}
